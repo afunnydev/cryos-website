ed1c36f51d81341103e554dbebbc7960e1fdd09a
Danny Boisvert <d.boisvert17@gmail.com>
2017-05-11 19:05:30 -0400

    Finished contact, finished emploi, finished faq, corrections to overall design inconsistencies

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,13 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
   $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrainWidth: true, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
-      stopPropagation: false // Stops event propagation
-    }
-  );
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: true, // Does not change width of dropdown to that of the activator
+    hover: true, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: true, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: false // Stops event propagation
+  });
+  $('.collapsible').collapsible();
 });
